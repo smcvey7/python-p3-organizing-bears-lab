3d62e33ba2a2cf8d0761a730995795bf62cb813a
Steven McVey <steven@stevenmcvey.com>
2024-02-27 19:11:50 -0500

    lab complete

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT into bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 1, "F", "brown", "bad", false);
+INSERT into bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 1, "F", "brown", "bad", false);
+INSERT into bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 1, "F", "brown", "bad", false);
+INSERT into bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 1, "F", "brown", "bad", false);
+INSERT into bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 1, "F", "brown", "bad", false);
+INSERT into bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 1, "F", "brown", "bad", false);
+INSERT into bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 1, "F", "brown", "bad", false);
+INSERT into bears (name, age, sex, color, temperament, alive) VALUES (NULL, 1, "F", "brown", "bad", false);
